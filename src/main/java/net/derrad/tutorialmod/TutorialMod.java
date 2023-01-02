3bd27cf9bd514596922a0a416ec2ae80fe197540
DerradTV <121821518+DerradTV@users.noreply.github.com>
2023-01-02 19:49:39 +0200

    Initial commit

---
+++ src/main/java/net/derrad/tutorialmod/TutorialMod.java
@@ -0,0 +1,102 @@
+package net.derrad.tutorialmod;
+
+import com.mojang.logging.LogUtils;
+import net.derrad.tutorialmod.block.ModBlocks;
+import net.derrad.tutorialmod.block.entity.ModBlockEntities;
+import net.derrad.tutorialmod.entity.ModEntityTypes;
+import net.derrad.tutorialmod.entity.client.ChomperRenderer;
+import net.derrad.tutorialmod.fluid.ModFluidTypes;
+import net.derrad.tutorialmod.fluid.ModFluids;
+import net.derrad.tutorialmod.item.ModItems;
+import net.derrad.tutorialmod.loot.ModLootModifiers;
+import net.derrad.tutorialmod.networking.ModMessages;
+import net.derrad.tutorialmod.painting.ModPaintings;
+import net.derrad.tutorialmod.recipe.ModRecipes;
+import net.derrad.tutorialmod.screen.GemInfusingStationScreen;
+import net.derrad.tutorialmod.screen.ModMenuTypes;
+import net.derrad.tutorialmod.villager.ModVillagers;
+import net.derrad.tutorialmod.world.feature.ModConfiguredFeatures;
+import net.derrad.tutorialmod.world.feature.ModPlacedFeatures;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.EntityRenderers;
+import net.minecraft.world.entity.SpawnPlacements;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.FlowerPotBlock;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+import software.bernie.geckolib3.GeckoLib;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(TutorialMod.MOD_ID)
+public class TutorialMod {
+    public static final String MOD_ID = "tutorialmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    // Very Important Comment
+    public TutorialMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
+
+        ModVillagers.register(modEventBus);
+        ModPaintings.register(modEventBus);
+
+        ModConfiguredFeatures.register(modEventBus);
+        ModPlacedFeatures.register(modEventBus);
+
+        ModFluids.register(modEventBus);
+        ModFluidTypes.register(modEventBus);
+
+        ModBlockEntities.register(modEventBus);
+        ModMenuTypes.register(modEventBus);
+
+        ModRecipes.register(modEventBus);
+        ModEntityTypes.register(modEventBus);
+
+        ModLootModifiers.register(modEventBus);
+
+        GeckoLib.initialize();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            SpawnPlacements.register(ModEntityTypes.CHOMPER.get(),
+                    SpawnPlacements.Type.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
+                    Monster::checkMonsterSpawnRules);
+
+            ((FlowerPotBlock) Blocks.FLOWER_POT).addPlant(ModBlocks.JASMINE.getId(), ModBlocks.POTTED_JASMINE);
+
+            ModMessages.register();
+            ModVillagers.registerPOIs();
+        });
+    }
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            ItemBlockRenderTypes.setRenderLayer(ModFluids.SOURCE_SOAP_WATER.get(), RenderType.translucent());
+            ItemBlockRenderTypes.setRenderLayer(ModFluids.FLOWING_SOAP_WATER.get(), RenderType.translucent());
+
+            MenuScreens.register(ModMenuTypes.GEM_INFUSING_STATION_MENU.get(), GemInfusingStationScreen::new);
+
+            EntityRenderers.register(ModEntityTypes.CHOMPER.get(), ChomperRenderer::new);
+        }
+    }
+}
